b8c40155fffd2e59b93ec6c77c5fa751af218379
dzozrawr <jovan_96@live.ru>
2021-07-21 20:43:22 +0200

    fixed player movement jitter (camera)

--- Assets/Scripts/TrailCamera.cs
+++ Assets/Scripts/TrailCamera.cs
@@ -8,22 +8,19 @@ public class TrailCamera : MonoBehaviour
     public Transform target;
     private float trailDistance = 5.0f;  //was public
     private float heightOffset = 3.0f;   //was public
-   // public float cameraDelay = 0.02f;
+                                         // public float cameraDelay = 0.02f;
 
     private void Start()
     {
-        heightOffset = transform.position.y- target.position.y;
+        heightOffset = transform.position.y - target.position.y;
         trailDistance = target.position.z - transform.position.z;
-       // transform.LookAt(target.transform); //camera looks at player (rotation)
+        // transform.LookAt(target.transform); //camera looks at player (rotation)
     }
 
-    void Update()
+    void LateUpdate()
     {
-        //Vector3 followPos = target.position - target.forward * trailDistance;
 
-        transform.position = new Vector3(transform.position.x, target.position.y+ heightOffset, target.position.z - trailDistance); //camera centered while following at a certain distance and following the player if he jumps
-       // transform.LookAt(target.transform);
-        //  followPos.y += heightOffset;
-        //transform.position += (followPos - transform.position) * cameraDelay;
+        transform.position = new Vector3(transform.position.x, target.position.y + heightOffset, target.position.z - trailDistance); //camera centered while following at a certain distance and following the player if he jumps
+
     }
 }
