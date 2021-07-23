90ae0ae39e665e20d507b47dfaf3509196597476
dzozrawr <jovan_96@live.ru>
2021-07-23 19:04:20 +0200

    added random tile generation

--- Assets/Scripts/SpawnTile.cs
+++ Assets/Scripts/SpawnTile.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class SpawnTile : MonoBehaviour
 {
     public GameObject tileToSpawn;
-    public GameObject referenceObject;
+    public GameObject[] tilesToSpawn;
+    public GameObject previousTile;
     private Vector3 previousTilePosition;
     private Vector3 direction = new Vector3(0, 0, 1);   // z direction (forward direction)
     private float tileLength;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        previousTilePosition = referenceObject.transform.position;
-        tileLength= referenceObject.transform.localScale.z;
+        previousTilePosition = previousTile.transform.position;
+        tileLength= previousTile.transform.localScale.z;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 checkVector= Camera.main.WorldToViewportPoint(previousTilePosition);
 
         if (checkVector.x>0&& checkVector.x < 1&& checkVector.y > 0 && checkVector.y < 1 && checkVector.z>0) //if previous tile is seen by the camera, make a new one in front of it
         {
+            GameObject nextTile = tilesToSpawn[Random.Range(0, tilesToSpawn.Length)];            
             Vector3 spawnPos = previousTilePosition + tileLength * direction;
-            Instantiate(tileToSpawn, spawnPos, Quaternion.Euler(0, 0, 0));
+            Instantiate(nextTile, spawnPos, Quaternion.Euler(0, 0, 0));
             previousTilePosition = spawnPos;
+            tileLength = nextTile.transform.localScale.z;
         }
 
     }
